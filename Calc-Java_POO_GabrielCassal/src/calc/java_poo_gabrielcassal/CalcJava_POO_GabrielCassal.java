fb4900bb2646f852a338b4f788e6364b093a3239
gabrcass <68290940+gabrcass@users.noreply.github.com>
2020-09-21 19:04:17 -0300

    Inicio

---
+++ Calc-Java_POO_GabrielCassal/src/calc/java_poo_gabrielcassal/CalcJava_POO_GabrielCassal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc.java_poo_gabrielcassal;
+
+/**
+ *
+ * @author gabrc
+ */
+public class CalcJava_POO_GabrielCassal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Frm_Home().setVisible(true);
+    }
+    
+}
